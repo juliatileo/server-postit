ccf9b45a1084e310fe22e2f44eec813ef5d68ecf
Lucas Juliati <vinnilucas4@gmail.com>
2020-06-13 15:57:20 -0300

    cors e ignorando env

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import "reflect-metadata";
 import * as express from "express";
 import * as bodyParser from "body-parser";
+import cors from "cors";
 import routes from "./routes";
 import { createConnection } from "typeorm";
 
 const app = express();
 createConnection();
+app.use(cors());
 app.use(bodyParser.json());
 app.use(routes);
 
